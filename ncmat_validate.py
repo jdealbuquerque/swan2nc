0cefb0e4ff86bf0a658971fde660858c6c98379f
joao <j.dealbuquerque@metocean.co.nz>
2023-01-16 22:46:44 +1300

    Final tests implemented, fingers crossed

--- ncmat_validate.py
+++ ncmat_validate.py
@@ -3,6 +3,8 @@ from swanfile2 import SwnData
 import xarray as xr
 import matplotlib.pyplot as plt
 import ipdb
+import os
+import numpy as np
 
 if __name__ == '__main__':
     import argparse
@@ -38,10 +40,33 @@ if __name__ == '__main__':
     sdataset = sdata.readswan(in_args.s, in_args.w)
     ndataset = xr.open_dataset(in_args.n)
 
-    ipdb.set_trace()
     for key in sdataset.keys():
         tmp = ndataset[key] - sdataset[key]
         tmp.max(axis = 0).plot()
         plt.title('MAX = {}'.format(tmp.max().data))
-        plt.savefig(fout.replace('.png','{}.png'.format(key)))
-        plt.close()
+        plt.savefig(fout.replace('.png','diff-{}.png'.format(key)))
+        plt.clf()
+
+        ndataset[key].isel(time=0).plot()
+        plt.savefig(fout.replace('.png','T0-{}.png'.format(key)))
+        plt.clf()
+        
+        ndataset[key].isel(time=-1).plot()
+        plt.savefig(fout.replace('.png','T1-{}.png'.format(key)))
+        plt.clf()
+
+        t1 = ndataset[key].isel(longitude=10, latitude=10)
+        t2 = sdataset[key].isel(longitude=10, latitude=10)
+        p1 = np.abs((t1-t2).values).sum()
+        ((t1-t2).values/t1).plot()
+        plt.savefig(fout.replace('.png','diffpct-{}.png'.format(key)))
+        plt.clf()
+
+        match = (
+                (ndataset.time.isel(time=0) == \
+                sdataset.time.isel(time=0)) &
+                (ndataset.time.isel(time=-1) == \
+                sdataset.time.isel(time=-1))
+                ).values
+
+        print('{}, absolute errors p1: {:1.5f} time match: {} '.format(os.path.basename(in_args.s),p1, match))
